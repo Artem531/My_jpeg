1c5d57771621b801431f3bb8cdab7dd2bb233a98
Artem Dranoshuk <a.dranoshuk@dev.local>
2020-05-02 19:03:11 +0300

    ugly final version

--- main.py
+++ main.py
@@ -3,29 +3,6 @@ import cv2
 from skimage.measure import compare_psnr
 
 dir = 'cats.bmp'
-img = cv2.imread(dir)
-cv2.imwrite("cats.png", img)
-cv2.imwrite("cats.jpg", img)
-
-ref_jpg = rgb2ycbcr(cv2.resize(cv2.imread("cats.jpg"), (512, 256))).astype(int)
-ref_png = rgb2ycbcr(cv2.resize(cv2.imread("cats.png"), (512, 256))).astype(int)
-
-ref_entropy_jpg = list(map(lambda i: entropy(ref_jpg[:, :, i].reshape(-1)), list(range(3))))
-ref_entropy_png = list(map(lambda i: entropy(ref_png[:, :, i].reshape(-1)), list(range(3))))
-print("ref_entropy_jpg: ", ref_entropy_jpg)
-print("ref_entropy_png: ", ref_entropy_png)
 
 a = jpeg(dir)
-simple_jpg = (a.img_after_simple_compress * 255).astype(int)
-simple_jpg[simple_jpg < 0] = 0
-ok_jpg = (a.img_after_ok_compression * 255).astype(int)
-ok_jpg[ok_jpg < 0] = 0
-
-My_simple_entropy_jpg = list(map(lambda i: entropy(simple_jpg[:, :, i].reshape(-1)), list(range(3))))
-My_ok_entropy_jpg = list(map(lambda i: entropy(ok_jpg[:, :, i].reshape(-1)), list(range(3))))
-print("My_simple_entropy_jpg: ", My_simple_entropy_jpg)
-print("My_ok_entropy_jpg: ", My_ok_entropy_jpg)
-
-#print("psnr", compare_psnr(ref_jpg, ok_jpg, data_range=None))
-print("psnr", psnr(ref_jpg, ok_jpg))
 a.check_DCT()
--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import cv2
 import copy
 import matplotlib.pyplot as plt
 
+
 def psnr(img1, img2):
     """
     Computes psnr.
@@ -10,35 +11,79 @@ def psnr(img1, img2):
     :param img2: second img
     :return: psnr
     """
-    mse = np.mean( (img1 - img2) ** 2 )
+    mse = np.mean((img1 - img2) ** 2)
     if mse == 0:
         return 100
     PIXEL_MAX = 255.0
     return 20 * np.log10(PIXEL_MAX / np.sqrt(mse))
 
+
+def get__BC_magnitude(val):
+    return np.floor(np.log2(abs(val) + 1)), val
+
+
+def AC_count_bits(array):
+    """
+    count full bits consumed for img array (AC coefficients)
+    :param array: (run, level) array
+    return (int): bits
+    """
+    unique_rows, counts = np.unique(array, axis=0, return_counts=True)
+    finale_bit_for_run = np.sum(counts) * 4
+    bits_for_level = [(4 + get__BC_magnitude(val[1])[0]) for val in unique_rows]
+    finale_bits_for_level = np.sum([level_bits * count for level_bits, count in zip(bits_for_level, counts)])
+
+    return finale_bit_for_run + finale_bits_for_level
+
+
+def differential_coding(array):
+    """
+    differential coding
+    return: differential code array
+    """
+    res = [array[0] - np.mean(array)]
+    for i in range(1, len(array)):
+        res.append(array[i] - array[i - 1])
+    return res
+
+
+def DC_count_bits(array):
+    """
+    count full bits consumed for img array (DC coefficients)
+    :param array: 1D-array of DC coefficients
+    return (int): bits
+    """
+    differential_code = differential_coding(array)
+    unique_vals, counts = np.unique(differential_code, axis=0, return_counts=True)
+    bits_for_val = [(4 + get__BC_magnitude(val)[0]) for val in unique_vals]
+    finale_bits_for_DC = np.sum([level_bits * count for level_bits, count in zip(bits_for_val, counts)])
+
+    return finale_bits_for_DC
+
+
 def code_seq(array):
     """
     Series Length Coding
+    return: (run, level) array
     """
     prev = array[0]
     count = 1
     res = []
     for i in array[1:]:
         if count > 15 and prev == 0 and i == prev:
-            res.append([4, i])
-            res.append([4, i])
-            res.append([4, i])
-            res.append([4, i])
+            res.append([15, 0])
             count = 1
             continue
 
-        if i == prev:
+        if i == prev and prev == 0:
             count += 1
             continue
         res.append([count, i])
         prev = i
         count = 1
+    res.append([0, 0])
     return res
+
 
 def entropy(array):
     """
@@ -55,22 +100,25 @@ def entropy(array):
     for i in probs:
         entropy -= i * np.log2(i)
 
-    return entropy #* len(unique_rows)
+    return entropy  # * len(unique_rows)
+
 
 def rgb2ycbcr(im):
     xform = np.array([[.299, .587, .114], [-.1687, -.3313, .5], [.5, -.4187, -.0813]])
     ycbcr = im.dot(xform.T)
-    ycbcr[:,:,[1,2]] += 128
+    ycbcr[:, :, [1, 2]] += 128
     return ycbcr
+
 
 def ycbcr2rgb(im):
     xform = np.array([[1, 0, 1.402], [1, -0.34414, -.71414], [1, 1.772, 0]])
     rgb = im.astype(np.float)
-    rgb[:,:,[1,2]] -= 128
+    rgb[:, :, [1, 2]] -= 128
     rgb = rgb.dot(xform.T)
     np.putmask(rgb, rgb > 255, 255)
     np.putmask(rgb, rgb < 0, 0)
     return rgb
+
 
 class jpeg():
     def __init__(self, dir, N=8, n=7, percentage=0):
@@ -86,13 +134,13 @@ class jpeg():
         self.img_after_simple_compress = self.do_full_rev_DCT_transform(self.img_DCT_after_simple_compress, N)
 
         self.quantization_matrix = self.get_quantization_matrix(percentage)
-        self.DCT_after_full_quantization = self.full_dequantization(self.full_quantization(self.img_DCT, self.quantization_matrix, N), self.quantization_matrix, N)
+        self.DCT_after_full_quantization = self.full_dequantization(
+            self.full_quantization(self.img_DCT, self.quantization_matrix, N), self.quantization_matrix, N)
         self.img_after_ok_compression = self.do_full_rev_DCT_transform(self.DCT_after_full_quantization, N)
 
-
         self.get_PSNR_percentage_graph(1)
-        self.get_entropy_percentage_graph(1)
-        self.get_PSNR_entropy_graph(1)
+        self.get_bits_percentage_graph(1)
+        self.get_PSNR_bits_graph(1)
 
     def get_quantization_matrix(self, percentage):
         """
@@ -115,11 +163,11 @@ class jpeg():
         quantization_matrix_50 = np.zeros((self.N, self.N))
         for i in range(self.N):
             for j in range(self.N):
-                quantization_matrix_50[i, j] = (i + j)*percentage + 1
+                quantization_matrix_50[i, j] = (i + j) * percentage + 1
 
         # quantization_matrix_50 = np.array(quantization_matrix_50)
 
-        #print(quantization_matrix_50.shape)
+        # print(quantization_matrix_50.shape)
         def clip(quantization_matrix):
             quantization_matrix[quantization_matrix > 255] = 255
             quantization_matrix[quantization_matrix < 1] = -1
@@ -135,16 +183,15 @@ class jpeg():
         :return: NxN 2d DCT matrix
         """
 
-        matrix = np.zeros((N,N))
-        matrix[0,:] = np.sqrt(1/N)
+        matrix = np.zeros((N, N))
+        matrix[0, :] = np.sqrt(1 / N)
         v_idxs = np.array(range(N))
         for u, u_axis in enumerate(matrix):
             if u == 0:
                 continue
 
-            matrix[u, :] = np.sqrt(2/N) * np.cos( ((2*v_idxs+1)*np.pi*u)/(2*N) )
+            matrix[u, :] = np.sqrt(2 / N) * np.cos(((2 * v_idxs + 1) * np.pi * u) / (2 * N))
         return matrix
-
 
     def get_img_matrix(self, dir, YCbCr_key=True):
         """
@@ -171,7 +218,7 @@ class jpeg():
         """
         channels = img_box.shape[2]
         for i in range(channels):
-            img_box[:,:, i] = np.dot(np.dot(DCT_matrix, img_box[:,:, i]), np.transpose(DCT_matrix))
+            img_box[:, :, i] = np.dot(np.dot(DCT_matrix, img_box[:, :, i]), np.transpose(DCT_matrix))
         return img_box
 
     def do_small_rev_DCT_transform(self, img_box, DCT_matrix):
@@ -184,7 +231,7 @@ class jpeg():
         """
         channels = img_box.shape[2]
         for i in range(channels):
-            img_box[:,:, i] = np.dot(np.dot( np.transpose(DCT_matrix), img_box[:,:, i]), DCT_matrix)
+            img_box[:, :, i] = np.dot(np.dot(np.transpose(DCT_matrix), img_box[:, :, i]), DCT_matrix)
         return img_box
 
     def do_full_DCT_transform(self, img, N):
@@ -195,14 +242,14 @@ class jpeg():
         :return: img after DCT
         """
         img = copy.deepcopy(img)
-        h, w = img.shape[0],img.shape[1]
+        h, w = img.shape[0], img.shape[1]
         for i in range(int(h / N)):
             i = i * N
             for j in range(int(w / N)):
                 j = j * N
                 # cv2.imshow("before", img[i:i+N,j:j+N, :])
                 # cv2.waitKey(0)
-                img[i:i+N,j:j+N, :] = self.do_small_DCT_transform(img[i:i+N,j:j+N, :], self.DCT_matrix)
+                img[i:i + N, j:j + N, :] = self.do_small_DCT_transform(img[i:i + N, j:j + N, :], self.DCT_matrix)
                 # cv2.imshow("after", img[i:i + N, j:j + N, :])
                 # cv2.waitKey(0)
                 a = 0
@@ -222,11 +269,10 @@ class jpeg():
                 j = j * N
                 # cv2.imshow("before", img_DCT[i:i+N,j:j+N, :])
                 # cv2.waitKey(0)
-                img_DCT[i:i+N,j:j+N] = self.do_small_rev_DCT_transform(img_DCT[i:i + N, j:j + N], self.DCT_matrix)
+                img_DCT[i:i + N, j:j + N] = self.do_small_rev_DCT_transform(img_DCT[i:i + N, j:j + N], self.DCT_matrix)
                 # cv2.imshow("after", img_DCT[i:i + N, j:j + N, :])
                 # cv2.waitKey(0)
         return img_DCT
-
 
     def simple_compression(self, img_DCT, n, N):
         """
@@ -240,7 +286,7 @@ class jpeg():
         """
 
         def set_to_zero(img_DCT_part, n):
-            img_DCT_part[N-n:,N-n:,:] = 0
+            img_DCT_part[N - n:, N - n:, :] = 0
             return img_DCT_part
 
         img_DCT_after_simple_compress = copy.deepcopy(img_DCT)
@@ -250,7 +296,8 @@ class jpeg():
             for j in range(int(w / N)):
                 j = j * N
 
-                img_DCT_after_simple_compress[i:i+N,j:j+N] = set_to_zero(img_DCT_after_simple_compress[i:i+N,j:j+N], n)
+                img_DCT_after_simple_compress[i:i + N, j:j + N] = set_to_zero(
+                    img_DCT_after_simple_compress[i:i + N, j:j + N], n)
 
         return img_DCT_after_simple_compress
 
@@ -283,7 +330,8 @@ class jpeg():
             for j in range(int(w / N)):
                 j = j * N
 
-                img_DCT_after_compress[i:i+N,j:j+N] = self.small_quantization(img_DCT_after_compress[i:i+N,j:j+N], quantization_matrix)
+                img_DCT_after_compress[i:i + N, j:j + N] = self.small_quantization(
+                    img_DCT_after_compress[i:i + N, j:j + N], quantization_matrix)
 
         return img_DCT_after_compress
 
@@ -316,31 +364,31 @@ class jpeg():
             for j in range(int(w / N)):
                 j = j * N
 
-                img_DCT_after_compress[i:i+N,j:j+N] = self.small_dequantization(img_DCT_after_compress[i:i+N,j:j+N], quantization_matrix)
+                img_DCT_after_compress[i:i + N, j:j + N] = self.small_dequantization(
+                    img_DCT_after_compress[i:i + N, j:j + N], quantization_matrix)
 
         return img_DCT_after_compress
 
-    def get_PSNR_entropy_graph(self, step):
+    def get_PSNR_bits_graph(self, step):
         """
-        plot entropy/PSNR graph
+        plot bits/PSNR graph
         :param step (int): plot step
         :return: None
         """
 
         fig = plt.figure()
-        plt.title("entropy/PSNR")
+        plt.title("bits/PSNR")
         ax1 = fig.add_subplot(1, 1, 1)
-        ax1.set_ylabel('entropy')
+        ax1.set_ylabel('bits')
         ax1.set_xlabel('PSNR')
 
-        plt.plot(self.PSNR, self.entropy_list[:, 0], label="Y entropy")
-        plt.plot(self.PSNR, self.entropy_list[:, 1], label="Cb entropy")
-        plt.plot(self.PSNR, self.entropy_list[:, 2], label="Cr entropy")
+        plt.plot(self.PSNR, self.bits_arr[:, 0], label="Y bits")
+        plt.plot(self.PSNR, self.bits_arr[:, 1], label="Cb bits")
+        plt.plot(self.PSNR, self.bits_arr[:, 2], label="Cr bits")
 
         plt.legend(loc='upper right', borderaxespad=0.)
-        plt.savefig("entropy-PSNR.jpg")
+        plt.savefig("bits-PSNR.jpg")
         plt.show()
-
 
     def walk(self, matrix):
         """
@@ -356,7 +404,7 @@ class jpeg():
             if len(diag) % 2:
                 diag.reverse()
             zigzag += diag
-        return zigzag
+        return zigzag[1:]
 
     # def walk(self, matrix):
     #     """
@@ -371,52 +419,62 @@ class jpeg():
         """
         Move all zeros to the end for all img
         :param DCT (numpy): DCT array
-        :return: DCT 1D-array with all zeros in the end
+        :return: seq (run, level) 1D-array for AC and 1D-array of DC coef
         """
         img_DCT_after_zig_zag = copy.deepcopy(img_DCT)
         res = []
+        DC_arr = []
         h, w = img_DCT.shape[0], img_DCT.shape[1]
         for i in range(int(h / N)):
             i = i * N
             for j in range(int(w / N)):
                 j = j * N
 
-                res += self.walk(img_DCT_after_zig_zag[i:i+N,j:j+N])
+                res += code_seq(self.walk(img_DCT_after_zig_zag[i:i + N, j:j + N]))
+                DC_arr.append(img_DCT_after_zig_zag[i, j])
+        res = np.array(res)
+        return res, DC_arr
 
-        return res
-
-
-    def get_entropy_percentage_graph(self, step):
+    def get_bits_percentage_graph(self, step):
         """
-        plot percentage/entropy graph
+        plot percentage/biys graph
         :param step (int): plot step
         :return: None
         """
-        entropy_list = []
+        bits_list = []
         for percentage in range(0, 100, step):
             quantization_matrix = self.get_quantization_matrix(percentage)
-            DCT_after_full_quantization = self.full_quantization(self.img_DCT, quantization_matrix, self.N)
-            DCT_after_zig_zag = list(map(lambda i: self.full_walk(DCT_after_full_quantization[:, :, i], self.N), list(range(3)) ))
-            DCT_after_zig_zag = np.array(DCT_after_zig_zag) * 255
-            #img_after_ok_compression = ycbcr2rgb(self.do_full_rev_DCT_transform(DCT_after_full_quantization, self.N) * 255)
-            entropy_jpg = list(map(lambda i: entropy(DCT_after_zig_zag[i]), list(range(3)) ))
-            entropy_list.append(entropy_jpg)
-        entropy_list = np.array(entropy_list)
+            DCT_after_full_quantization = self.full_quantization(self.img_DCT, quantization_matrix, self.N) * 255
+            DCT_after_zig_zag = list(
+                map(lambda i: self.full_walk(DCT_after_full_quantization[:, :, i], self.N), list(range(3))))
+            AC_seq_arr = []
+            DC_arr = []
+            for i in DCT_after_zig_zag:
+                AC_seq_arr.append(i[0])
+                DC_arr.append(i[1])
+
+            AC_seq_arr = np.array(AC_seq_arr)
+            # img_after_ok_compression = ycbcr2rgb(self.do_full_rev_DCT_transform(DCT_after_full_quantization, self.N) * 255)
+            AC_full_bits_arr = np.array(list(map(lambda i: AC_count_bits(AC_seq_arr[i]), list(range(3)))))
+            DC_full_bits_arr = np.array(list(map(lambda i: DC_count_bits(DC_arr[i]), list(range(3)))))
+            full_bits_arr = AC_full_bits_arr + DC_full_bits_arr
+            bits_list.append(full_bits_arr)
+        bits_arr = np.array(bits_list)
 
         fig = plt.figure()
-        plt.title("percentage/entropy")
+        plt.title("percentage/bits")
         ax1 = fig.add_subplot(1, 1, 1)
         ax1.set_xlabel('R')
-        ax1.set_ylabel('entropy')
+        ax1.set_ylabel('bits')
 
-        plt.plot(range(0, 100, step), entropy_list[:, 0], label="Y entropy")
-        plt.plot(range(0, 100, step), entropy_list[:, 1], label="Cb entropy")
-        plt.plot(range(0, 100, step), entropy_list[:, 2], label="Cr entropy")
+        plt.plot(range(0, 100, step), bits_arr[:, 0], label="Y bits")
+        plt.plot(range(0, 100, step), bits_arr[:, 1], label="Cb bits")
+        plt.plot(range(0, 100, step), bits_arr[:, 2], label="Cr bits")
 
         plt.legend(loc='upper right', borderaxespad=0.)
-        plt.savefig("percentage-entropy.jpg")
+        plt.savefig("percentage-bits.jpg")
         plt.show()
-        self.entropy_list = entropy_list
+        self.bits_arr = bits_arr
 
     def get_PSNR_percentage_graph(self, step):
         """
@@ -426,12 +484,13 @@ class jpeg():
         """
         PSNR = []
         img_DCT = copy.deepcopy(self.img_DCT)
-        ref = ycbcr2rgb(self.img * 255)
+        ref = self.img * 255
 
         for percentage in range(0, 100, step):
             quantization_matrix = self.get_quantization_matrix(percentage)
-            DCT_after_full_quantization = self.full_dequantization( self.full_quantization(img_DCT, quantization_matrix, self.N) , quantization_matrix, self.N)
-            img_after_ok_compression = ycbcr2rgb(self.do_full_rev_DCT_transform(DCT_after_full_quantization, self.N) * 255)
+            DCT_after_full_quantization = self.full_dequantization(
+                self.full_quantization(img_DCT, quantization_matrix, self.N), quantization_matrix, self.N)
+            img_after_ok_compression = self.do_full_rev_DCT_transform(DCT_after_full_quantization, self.N) * 255
             PSNR_jpg = psnr(img_after_ok_compression, ref)
             PSNR.append(PSNR_jpg)
 
